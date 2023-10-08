d9f8920e1b5b58c597c7e48fdf2ef911932ffc61
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-10-08 14:55:05 +0300

    Start project

---
+++ src/main/java/com/example/shortlinkgenerator/ShortLinkGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortlinkgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkGeneratorApplication.class, args);
+    }
+
+}
